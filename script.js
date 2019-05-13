c3abec17d14a5318edb5dc43e70316da1c915e86
Shubham Makwana <50435956+shubhammak@users.noreply.github.com>
2019-05-13 18:06:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+(function () {
+
+
+    window.addEventListener("resize", edualHeight);
+    window.addEventListener("load", edualHeight);
+
+    function edualHeight() {
+        var domSelector = document.querySelectorAll('.equalHeight');
+        className = [];
+        Height = [];
+        domSelector.forEach(function (ele) {
+            if (className.indexOf(ele.parentNode.className.split(' ')[0]) == -1) { className.push(ele.parentNode.className.split(' ')[0]); }
+        });
+        className.forEach(function (ele) {
+            var NodeList = document.querySelectorAll('.' + ele);
+            NodeList.forEach(function (element, index) {
+                for (let i = 0; i < element.children.length; i++) {
+                    Height.push(element.children[i].clientHeight);
+                }
+                for (let i = 0; i < element.children.length; i++) {
+                    element.children[i].style.height = Math.max.apply(null, Height) + "px";
+                }
+                Height = [];
+            });
+        });
+    }
+
+})()
